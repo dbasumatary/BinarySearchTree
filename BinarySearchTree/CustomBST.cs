48e45be38a0f03444855844952ece5086d0d4e85
dbasumatary <basudhondip@gmail.com>
2023-06-06 01:53:19 +0530

    Added: Add method in Binary Search Tree

---
+++ BinarySearchTree/CustomBST.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class CustomBST
+    {
+        private class Node
+        {
+            public int Data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                this.Data = data;
+            }
+        }
+        private Node Root;
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if(Root == null)
+            {
+                Root = newNode;
+                return;
+            }
+            Node temp = Root;
+
+            while(temp != null)
+            {
+                if (newNode.Data < temp.Data)
+                {
+                    if (temp.left == null)
+                    {
+                        temp.left = newNode;
+                        return;
+                    }
+                    temp = temp.left;
+                }
+                else if (newNode.Data > temp.Data)
+                {
+                    if(temp.right == null)
+                    {
+                        temp.right = newNode;
+                        return;
+                    }
+                    temp = temp.right;
+                }
+                else
+                {
+                    Console.WriteLine($"{data} is already present in the tree.");
+                    return;
+                }
+            }
+            
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Binary Search Tree\n");
+            CustomBST customObject = new CustomBST();
+
+            customObject.Add(67);
+            customObject.Add(45);
+            customObject.Add(61);
+            customObject.Add(38);
+            customObject.Add(51);
+            customObject.Add(64);
+            customObject.Add(81);
         }
     }
 }
